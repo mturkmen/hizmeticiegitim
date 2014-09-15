370d6470621fbcdea2c22988c5107e27e54ce064
Vildan Ilkı <vildan.ilki@eba.gov.tr>
2014-09-16 00:27:27 +0300

    egitim gun #1

---
+++ src/tr/gov/eba/egitim/MainActivity.java
@@ -0,0 +1,61 @@
+package tr.gov.eba.egitim;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	TextView txt1;
+	Button btn1;
+	
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        txt1 = (TextView) findViewById(R.id.textView1);
+        btn1 = (Button)   findViewById(R.id.button1);
+        btn1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(),"Butona bastõnõz!" , Toast.LENGTH_SHORT).show();
+				txt1.setText("Merhaba butonuna basõldõ.");
+			}
+		});
+        
+        
+        
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
